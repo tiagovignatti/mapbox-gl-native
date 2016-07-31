c703b321729223d92334c3cf7ae9e5aeb685367b
Ivo van Dongen <info@ivovandongen.nl>
2016-07-31 18:47:13 -0400

    [android] #5610 - use Rule instead of deprecated base class for unit tests

--- platform/android/MapboxGLAndroidSDKTestApp/src/androidTest/java/com/mapbox/mapboxsdk/style/RuntimeStyleTests.java
+++ platform/android/MapboxGLAndroidSDKTestApp/src/androidTest/java/com/mapbox/mapboxsdk/style/RuntimeStyleTests.java
@@ -1,41 +1,36 @@
 package com.mapbox.mapboxsdk.style;
 
-import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.ActivityInstrumentationTestCase2;
 
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.style.layers.FillLayer;
 import com.mapbox.mapboxsdk.style.layers.NoSuchLayerException;
+import com.mapbox.mapboxsdk.style.layers.Property;
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
 import com.mapbox.mapboxsdk.style.sources.VectorSource;
 import com.mapbox.mapboxsdk.testapp.activity.style.RuntimeStyleTestActivity;
 
 import org.junit.After;
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
 
 /**
  * Basic smoke tests for Layer and Source
  */
 @RunWith(AndroidJUnit4.class)
-public class RuntimeStyleTests
-        extends ActivityInstrumentationTestCase2<RuntimeStyleTestActivity> {
+public class RuntimeStyleTests {
 
-    public RuntimeStyleTests() {
-        super(RuntimeStyleTestActivity.class);
-    }
-
-    @Before
-    public void setUp() throws Exception {
-        super.setUp();
-        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
-    }
+    @Rule
+    public final ActivityTestRule<RuntimeStyleTestActivity> rule = new ActivityTestRule<>(RuntimeStyleTestActivity.class);
 
     @Test
     public void testGetAddRemoveLayer() {
-        getActivity().mapView.getMapAsync(new OnMapReadyCallback() {
+        rule.getActivity().mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(MapboxMap mapboxMap) {
                 //Get initial
@@ -55,23 +50,25 @@ public class RuntimeStyleTests
                 mapboxMap.addLayer(layer);
 
                 assertNotNull(mapboxMap.getLayer("building"));
+
+                try {
+                    layer.set(PropertyFactory.visibility(Property.VISIBLE));
+                    assertTrue("Never reached as the reference is invalid after adding", false);
+                } catch (Exception e) {
+                    //Expected, reference is no longer valid
+                }
             }
         });
     }
 
     @Test
     public void testAddRemoveSource() {
-        getActivity().mapView.getMapAsync(new OnMapReadyCallback() {
+        rule.getActivity().mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(MapboxMap mapboxMap) {
                 mapboxMap.addSource(new VectorSource("my-source", "mapbox://mapbox.mapbox-terrain-v2"));
                 mapboxMap.removeSource("my-source");
             }
         });
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        super.tearDown();
     }
 }
